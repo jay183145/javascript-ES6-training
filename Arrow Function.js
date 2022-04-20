3e3362416122075e7adc56133695eb341afb0d1f
jay183145 <jay183145@gmail.com>
2022-04-20 15:25:33 +0800

    question 1

--- Arrow Function.js
+++ Arrow Function.js
@@ -2,6 +2,10 @@
 //     console.log('1S pass');    
 // }, 1000);
 
-setTimeout(()=>{
-    console.log("經過一秒");
-},1000);
+// setTimeout(()=>{
+//     console.log("經過一秒");
+// },1000);
+
+let f=()=>(5);
+let result = f();
+console.log(result);
