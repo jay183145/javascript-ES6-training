9d41b508dbe809dd6ddf8ba3b6fe2b83f2d509bf
jay183145 <jay183145@gmail.com>
2022-04-20 16:26:00 +0800

    question 1

--- Default Parameter.js
+++ Default Parameter.js
@@ -7,8 +7,14 @@
 // show("Hello");
 // show();
 
-let show=(message="預設值")=>{
-    console.log(message);
-};
-show("Hello");
-show();
+// let show=(message="預設值")=>{
+//     console.log(message);
+// };
+// show("Hello");
+// show();
+
+let f=(n1,n2=5)=>{
+    console.log(n1*n2);
+}
+f(3,1);
+f(8);
