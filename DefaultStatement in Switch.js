b59e2a39f8f25bc925c5e6a65795da0dfd8d99f5
jay183145 <jay183145@gmail.com>
2022-04-21 17:03:08 +0800

    DefaultStatement in Switch

---
+++ DefaultStatement in Switch.js
@@ -0,0 +1,20 @@
+function switchOfStuff(val) {
+    let answer = '';
+    switch (val){
+      case 'a':
+        return 'apple';
+        break;
+      case 'b':
+        return 'bird';
+        break;
+      case 'c':
+        return 'cat';
+        break;
+      default:
+        return "stuff";
+        break;
+    }
+    return answer;
+  }
+  
+  switchOfStuff('a');
