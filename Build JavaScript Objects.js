494191769ae687b763e42f0cbb607ccd6ba5c3c4
jay183145 <jay183145@gmail.com>
2022-04-22 09:43:07 +0800

    Build Javascript Object

---
+++ Build JavaScript Objects.js
@@ -0,0 +1,18 @@
+// if your object has any non-string properties, JavaScript will automatically typecast them as strings.
+
+// const cat = {
+//     "name": "Whiskers",
+//     "legs": 4,
+//     "tails": 1,
+//     "enemies": ["Water", "Dogs"]
+//   };
+
+const myDog = {
+    "name": "Bark Bark",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Me","Pete","Ro"],
+  };
+
+  let doggyname = myDog.name;
+  console.log(doggyname);
