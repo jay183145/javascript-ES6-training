2a999f9e346440a818737aea85553cf07bcd5603
jay183145 <jay183145@gmail.com>
2022-04-25 10:34:14 +0800

    Iterate Through an Array with a For Loops

---
+++ For Loops/Iterate Through an Array with a For Loop.js
@@ -0,0 +1,11 @@
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+let total = 0;
+// don't declare in the for loops or it will initialized every time it loops
+
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+  console.log(total);
+}
+
