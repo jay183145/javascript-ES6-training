3cd6658dda7bcb448cdb5ad57afa4231606a0bf8
jay183145 <jay183145@gmail.com>
2022-04-25 10:08:35 +0800

    Iterate with JavaScript For Loops

---
+++ For Loops/Iterate with JavaScript For Loops.js
@@ -0,0 +1,9 @@
+// for (a; b; c), where a is the initialization statement, b is the condition statement, and c is the final expression.
+//  if the condition starts as false, your loop will never execute.
+// Setup
+
+const myArray = [];
+
+for (let i = 1; i <6; i++) {
+  myArray.push(i);
+}
