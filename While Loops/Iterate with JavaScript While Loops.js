da3a1d5b88877a98b5b98beecd44f01a3889e4b5
jay183145 <jay183145@gmail.com>
2022-04-25 09:24:31 +0800

    Iterate with JavaScript While Loops

---
+++ While Loops/Iterate with JavaScript While Loops.js
@@ -0,0 +1,12 @@
+// The first type of loop we will learn is called a while loop because it runs while a specified condition is true and stops once that condition is no longer true.
+
+const myArray = [];
+
+let i = 5;
+
+while (i>=0) {
+  myArray.push(i);
+  i--;
+}
+
+console.log(myArray);
