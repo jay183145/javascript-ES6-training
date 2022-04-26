bce54c5685fb37a7e452e8908f7995c04ee3fd94
jay183145 <jay183145@gmail.com>
2022-04-26 10:53:23 +0800

    Generate Random Fractions with JavaScript

---
+++ Math random/Generate Random Fractions with JavaScript.js
@@ -0,0 +1,5 @@
+// JavaScript has a Math.random() function that generates a random decimal number between 0 (inclusive) and 1 (exclusive). Thus Math.random() can return a 0 but never return a 1.
+
+function randomFraction() {  
+    return Math.random();
+  }
