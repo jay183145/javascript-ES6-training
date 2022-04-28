efa1c421c17a119082c3b2417fbd86040160d43e
jay183145 <jay183145@gmail.com>
2022-04-28 13:39:07 +0800

    Reversed Strings(for loop)

---
+++ Codewar/Reverse String/Reversed Strings(for loop).js
@@ -0,0 +1,8 @@
+let reverseFor = (string) => {
+    let newString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        newString += string[i];
+    }
+    return newString;
+}
+console.log(reverseFor("Hello"));
