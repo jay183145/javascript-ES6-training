9ef62610c67ec375c7f3f33cd7d21a3ef5f6ee54
jay183145 <jay183145@gmail.com>
2022-04-29 09:04:42 +0800

    MakeUpperCase

---
+++ Codewar/MakeUpperCase.js
@@ -0,0 +1,10 @@
+function makeUpperCase(str) {
+    let upStr = str.toUpperCase();
+    return upStr;
+  }
+
+// function makeUpperCase(str) {
+//     return str.toUpperCase();
+//   }
+
+console.log(makeUpperCase("Hello"));
