6810aa738ac9dd9c6f1daff3dc07dcde7fb5ddbf
jay183145 <jay183145@gmail.com>
2022-04-29 09:37:33 +0800

    Use Destructuring Assignment to Pass an Object as a Function's Parameters

---
+++ Destructuring Assignment & Spread Operater/Use Destructuring Assignment to Pass an Object as a Function's Parameters.js
@@ -0,0 +1,16 @@
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+  };
+  
+
+//   const {max, min} = stats
+//   const half = (stats) => (max + min) / 2.0; 
+
+  const half = ({max, min}) => (max + min) / 2.0; 
+
+  console.log(half(stats));
