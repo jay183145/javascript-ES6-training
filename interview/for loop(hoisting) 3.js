5408f5faa945fd7ae93ac2a33960f5f3834daa23
藍天駿 <jay183145@gmail.com>
2022-05-25 12:52:46 +0800

    for loop 3

---
+++ interview/for loop(hoisting) 3.js
@@ -0,0 +1,16 @@
+const arr = [100, 200, 300];
+
+
+   
+
+for (var i = 0; i < 3; i++) {
+  callfun(i)
+  // console.log(i)
+}
+function callfun(params) {
+  setTimeout(() => console.log(params), arr[i])  
+}
+
+
+
+
